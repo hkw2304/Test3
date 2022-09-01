e759d8039274b788f93eac5b1fbc17ea5ffe3f2c
hkw <MSI@DESKTOP-S3M0BRN>
2022-09-01 09:36:04 +0900

    qqqq

--- src/exam02/Exam.java
+++ src/exam02/Exam.java
@@ -3,7 +3,7 @@ package exam02;
 public class Exam {
 
 public static void main(String[] args) {
-		System.out.println("hello world!!!");
+		System.out.println("sssssshello world!!!");
 		System.out.println("hello world!!!");System.out.println("hello world!!!");System.out.println("hello world!!!");
 	}
 }
