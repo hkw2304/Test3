db324e295ad7372c4f97d8ef3da1350b02d1a237
hkw <MSI@DESKTOP-S3M0BRN>
2022-09-01 09:35:50 +0900

    ssss

---
+++ src/exam02/Exam.java
@@ -0,0 +1,9 @@
+package exam02;
+
+public class Exam {
+
+public static void main(String[] args) {
+		System.out.println("hello world!!!");
+		System.out.println("hello world!!!");System.out.println("hello world!!!");System.out.println("hello world!!!");
+	}
+}
